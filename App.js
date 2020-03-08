d40fda4832882249a540c8b14d46e31efb246f26
gaurav roy <rockingroy2009@gmail.com>
2020-03-08 16:25:28 +0530

    did changs to text2speech

--- App.js
+++ App.js
@@ -9,203 +9,242 @@ import {
   Image,
   PanResponder,
   Animated,
+  Button,
+  ScrollView,
+  TouchableHighlight,
 } from 'react-native';
+import Video from 'react-native-video';
+import Voice from '@react-native-community/voice';
 
-import MapView, {
-  ProviderPropType,
-  Marker,
-  AnimatedRegion,
-  Polyline,
-} from 'react-native-maps';
-import carImage from './carImage.png';
-
-const screen = Dimensions.get('window');
-
-const ASPECT_RATIO = screen.width / screen.height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-
-class AnimatedMarkers extends React.Component {
+export default class VoiceNative extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      // coordinate: new AnimatedRegion({
-      //   latitude: 37.78825,
-      //   longitude: -122.4324,
-      // }),
-      coordinate: {
-        latitude: 37.78825,
-        longitude: -122.4324,
-      },
-      curPos: {latitude: 37.420814, longitude: -122.081949},
-      prevPos: null,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-      curAng: 45,
-      curPosArr: [{latitude: 37.420814, longitude: -122.081949}],
-      pan: new Animated.ValueXY(),
+      recognized: '',
+      pitch: '',
+      error: '',
+      end: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      buttonText: 'Search',
+      videoUrls: [
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4',
+      ],
+      videoIndex: 0,
     };
+
+    Voice.onSpeechStart = this.onSpeechStart;
+    Voice.onSpeechRecognized = this.onSpeechRecognized;
+    Voice.onSpeechEnd = this.onSpeechEnd;
+    Voice.onSpeechError = this.onSpeechError;
+    Voice.onSpeechResults = this.onSpeechResults;
+    Voice.onSpeechPartialResults = this.onSpeechPartialResults;
+    Voice.onSpeechVolumeChanged = this.onSpeechVolumeChanged;
+  }
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
   }
 
-  componentWillMount() {
-    this._panResp = PanResponder.create({
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderMove: Animated.event([
-        null,
-        {dx: this.state.pan.x, dy: this.state.pan.y},
-      ]),
-    });
-  }
-  // animate() {
-  //   const {coordinate} = this.state;
-  //   const newCoordinate = {
-  //     latitude: LATITUDE + (Math.random() - 0.5) * (LATITUDE_DELTA / 2),
-  //     longitude: LONGITUDE + (Math.random() - 0.5) * (LONGITUDE_DELTA / 2),
-  //   };
-
-  //   if (Platform.OS === 'android') {
-  //     if (this.marker) {
-  //       this.marker._component.animateMarkerToCoordinate(newCoordinate, 500);
-  //     }
-  //   } else {
-  //     coordinate.timing(newCoordinate).start();
-  //   }
-  // }
-
-  changePosition = (latOffset, lonOffset) => {
-    const latitude = this.state.curPos.latitude + latOffset;
-    const longitude = this.state.curPos.longitude + lonOffset;
-    let currPosArr = [...this.state.curPosArr];
-    currPosArr.push({latitude, longitude});
+  onSpeechStart = e => {
+    console.log('onSpeechStart: ', e);
     this.setState({
-      prevPos: this.state.curPos,
-      curPos: {latitude, longitude},
-      curPosArr: currPosArr,
+      started: '√',
+      buttonText: 'Listening...',
     });
-    this.updateMap();
   };
 
-  getRotation = (prevPos, curPos) => {
-    if (!prevPos) {
-      return 0;
+  onSpeechRecognized = e => {
+    console.log('onSpeechRecognized: ', e);
+    this.setState({
+      recognized: '√',
+    });
+  };
+
+  onSpeechEnd = e => {
+    console.log('onSpeechEnd: ', this.state.partialResults);
+    this.setState({
+      end: '√',
+      buttonText: 'Search Again',
+    });
+  };
+
+  onSpeechError = e => {
+    console.log('onSpeechError: ', e);
+    this.setState({
+      error: JSON.stringify(e.error),
+    });
+  };
+
+  onSpeechResults = e => {
+    console.log('onSpeechResults: ', e);
+    this.setState({
+      results: e.value,
+    });
+  };
+
+  onSpeechPartialResults = e => {
+    console.log('onSpeechPartialResults: ', e);
+    let index = 0;
+    let searchText = e.value[0].toLowerCase();
+    if (searchText.includes('cat')) {
+      index = 1;
     }
-    const xDiff = curPos.latitude - prevPos.latitude;
-    const yDiff = curPos.longitude - prevPos.longitude;
-    return (Math.atan2(yDiff, xDiff) * 180.0) / Math.PI;
+    if (searchText.includes('dog')) {
+      index = 2;
+    }
+    this.setState({
+      partialResults: e.value,
+      videoIndex: index,
+    });
   };
 
-  updateMap = () => {
-    const {curPos, prevPos, curAng} = this.state;
-    const curRot = this.getRotation(prevPos, curPos);
-    this.map.animateCamera({heading: curRot, center: curPos, pitch: curAng});
+  onSpeechVolumeChanged = e => {
+    console.log('onSpeechVolumeChanged: ', e);
+    this.setState({
+      pitch: e.value,
+    });
   };
 
+  _startRecognizing = async () => {
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+
+    try {
+      await Voice.start('en-US');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _stopRecognizing = async () => {
+    try {
+      await Voice.stop();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _cancelRecognizing = async () => {
+    try {
+      await Voice.cancel();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _destroyRecognizer = async () => {
+    try {
+      await Voice.destroy();
+    } catch (e) {
+      console.error(e);
+    }
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+  };
   render() {
+    let {videoIndex, videoUrls} = this.state;
     return (
-      <View style={styles.container}>
-        <MapView
-          provider={this.props.provider}
-          style={styles.map}
-          minZoomLevel={15}
-          initialRegion={{
-            ...this.state.curPos,
-            latitudeDelta: this.state.latitudeDelta,
-            longitudeDelta: this.state.longitudeDelta,
-          }}
-          ref={el => (this.map = el)}>
-          <MapView.Marker
-            coordinate={this.state.curPos}
-            anchor={{x: 0.5, y: 0.5}}>
-            <View>
-              <Image
-                source={require('./carImage.png')}
-                style={{height: 20, width: 20}}
-              />
-            </View>
-          </MapView.Marker>
-          <Polyline
-            coordinates={this.state.curPosArr}
-            strokeColor="#B24112" // fallback for when `strokeColors` is not supported by the map-provider
-            strokeColors={[
-              '#7F0000',
-              '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
-              '#B24112',
-              '#E5845C',
-              '#238C23',
-              '#7F0000',
-            ]}
-            strokeWidth={6}
-          />
-        </MapView>
-        <Animated.View
-          style={[
-            styles.buttonContainer,
-            {
-              transform: [
-                {
-                  translateX: this.state.pan.x,
-                  translateY: this.state.pan.y,
-                },
-              ],
-            },
-          ]}
-          {...this._panResp.panHandlers}>
-          <TouchableOpacity
-            onPress={() => this.changePosition(-0.0003, 0)}
-            style={[styles.bubble, styles.button]}>
-            <Text>Animate</Text>
-          </TouchableOpacity>
-        </Animated.View>
-      </View>
+      <ScrollView style={{flex: 1}}>
+        <Video
+          source={{
+            uri: videoUrls[videoIndex],
+          }} // Can be a URL or a local file.
+          ref={ref => {
+            this.player = ref;
+          }} // Store reference
+          style={styles.backgroundVideo}
+          controls={true}
+          resizeMode="cover"
+        />
+
+        <Text style={styles.stat}>Your search results will appear here</Text>
+        {this.state.partialResults.map((result, index) => {
+          return (
+            <Text key={`partial-result-${index}`} style={styles.stat}>
+              {result}
+            </Text>
+          );
+        })}
+
+        <TouchableHighlight
+          onPress={this._startRecognizing}
+          style={{
+            width: 100,
+            backgroundColor: '#00FFFF',
+            borderRadius: 10,
+            alignSelf: 'center',
+          }}>
+          <Text
+            style={{
+              alignSelf: 'center',
+              fontWeight: 'bold',
+              color: '#fff',
+              paddingVertical: 10,
+            }}>
+            {this.state.buttonText}{' '}
+          </Text>
+        </TouchableHighlight>
+      </ScrollView>
     );
   }
 }
-
-AnimatedMarkers.propTypes = {
-  provider: ProviderPropType,
-};
-
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  // map: {
-  //   ...StyleSheet.absoluteFillObject,
-  // },
-  bubble: {
+  transcript: {
+    textAlign: 'center',
+    color: '#B0171F',
+    marginBottom: 1,
+    top: '400%',
     flex: 1,
-    backgroundColor: 'rgba(255,255,255,0.7)',
-    paddingHorizontal: 18,
-    paddingVertical: 12,
-    borderRadius: 20,
   },
-  latlng: {
-    width: 200,
-    alignItems: 'stretch',
+  backgroundVideo: {
+    height: 500,
   },
   button: {
-    width: 80,
-    paddingHorizontal: 12,
+    width: 50,
+    height: 50,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    marginHorizontal: 10,
+    backgroundColor: '#F5FCFF',
   },
-  buttonContainer: {
-    flexDirection: 'row',
-    marginVertical: 20,
-    backgroundColor: 'transparent',
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+  action: {
+    textAlign: 'center',
+    color: '#0000FF',
+    marginVertical: 5,
+    fontWeight: 'bold',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  stat: {
+    textAlign: 'center',
+    color: '#B0171F',
+    marginBottom: 1,
   },
 });
-
-export default AnimatedMarkers;
